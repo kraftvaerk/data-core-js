082071ddcb5dbf4129a5982f7acdeac17789f7e9
NinjaWinja <31620185+NinjaWinja@users.noreply.github.com>
2019-01-18 14:18:13 +0100

    Update global.api.js
    * endpoint data was ignored;

--- src/api/global.api.js
+++ src/api/global.api.js
@@ -7,6 +7,7 @@ const api = {
 
     call: (endpoint = null, data, type = 'json') => {
         if(!endpoint) throw new TypeError('api.call(endpoint, type) requries a valid endpoint object', endpoint);
+        if(data) endpoint.data = data;
         return _loader.do(endpoint).then(_sanitize[type]);
     }
 
